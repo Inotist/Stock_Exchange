97fdec80381c4c430c95273efcc06f15be83734c
Antonio Rabasco <43301727+Inotist@users.noreply.github.com>
2020-08-24 17:49:44 +0200

    change files type to plain text

--- App/get_data.py
+++ App/get_data.py
@@ -23,10 +23,12 @@ def read_dataset(symbol, last_date):
     bucket = storage_client.get_bucket(env["BUCKET"])
     blob = bucket.blob(f'datasets/{symbol}-{last_date}.csv')
 
-    if blob.exists(): data = read_csv(f'gs://{env["BUCKET"]}/datasets/{symbol}-{last_date}.csv').sort_values(by='timestamp')
-    else: data = get_daily_dataset(symbol, last_date).sort_values(by='timestamp')
+    if blob.exists():
+        data = read_csv(f'gs://{env["BUCKET"]}/datasets/{symbol}-{last_date}.csv')
+    else:
+        data = get_daily_dataset(symbol, last_date)
 
-    data = data.drop(columns='timestamp').to_numpy()
+    data = data.sort_values(by='timestamp').drop(columns='timestamp').to_numpy()
     
     return data
 
@@ -35,8 +37,11 @@ def read_predictions(symbol, last_date):
     bucket = storage_client.get_bucket(env["BUCKET"])
     blob = bucket.blob(f'predictions/{symbol}-{last_date}.json')
 
-    if blob.exists(): predictions = blob.download_as_string()
-    else: predictions = generate_predictions(symbol, last_date)
+    if blob.exists():
+        predictions = blob.download_as_string()
+        predictions = predictions.decode()
+    else:
+        predictions = generate_predictions(symbol, last_date)
 
     predictions = fromstring(re.sub('[\[\]\\n]', '', predictions).strip(), dtype=float, sep=' ').reshape(30,5)
 
